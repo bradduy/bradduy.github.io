3c9ce035c492a12f1a42351d42a4f21d60faa6cc
Duy Thanh Tran <bradduy@icloud.com>
2021-01-24 10:55:04 +0900

    change animation

--- js/scripts.js
+++ js/scripts.js
@@ -42,8 +42,8 @@
 })(jQuery); // End of use strict
 
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml2');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+var textWrapperMl2 = document.querySelector('.ml2');
+textWrapperMl2.innerHTML = textWrapperMl2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({ loop: true })
     .add({
@@ -66,58 +66,21 @@ $('body').bind('copy paste', function (e) {
     e.preventDefault(); return false;
 });
 
-var ml4 = {};
-ml4.opacityIn = [0, 1];
-ml4.scaleIn = [0.2, 1];
-ml4.scaleOut = 3;
-ml4.durationIn = 800;
-ml4.durationOut = 600;
-ml4.delay = 500;
+// Wrap every letter in a span
+var textWrapperMl9 = document.querySelector('.ml9 .letters');
+textWrapperMl9.innerHTML = textWrapperMl9.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({ loop: true })
-    .add({
-        targets: '.ml4 .letters-1',
-        opacity: ml4.opacityIn,
-        scale: ml4.scaleIn,
-        duration: ml4.durationIn
-    }).add({
-        targets: '.ml4 .letters-1',
-        opacity: 0,
-        scale: ml4.scaleOut,
-        duration: ml4.durationOut,
-        easing: "easeInExpo",
-        delay: ml4.delay
-    }).add({
-        targets: '.ml4 .letters-2',
-        opacity: ml4.opacityIn,
-        scale: ml4.scaleIn,
-        duration: ml4.durationIn
-    }).add({
-        targets: '.ml4 .letters-2',
-        opacity: 0,
-        scale: ml4.scaleOut,
-        duration: ml4.durationOut,
-        easing: "easeInExpo",
-        delay: ml4.delay
-    }).add({
-        targets: '.ml4',
-        opacity: 0,
-        duration: 500,
-        delay: 500
-    });
-
-anime.timeline({ loop: true })
-    .add({
-        targets: '.ml15 .word',
-        scale: [14, 1],
-        opacity: [0, 1],
-        easing: "easeOutCirc",
-        duration: 800,
-        delay: (el, i) => 800 * i
-    }).add({
-        targets: '.ml15',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-    });
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
